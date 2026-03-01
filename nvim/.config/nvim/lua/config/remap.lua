0727e18253f331c7a82505d0b8d880f96769626a
hyzoor <mkiii@hotmail.es>
2026-03-01 22:53:13 +0100

    i3 config v0

--- nvim/.config/nvim/lua/config/remap.lua
+++ nvim/.config/nvim/lua/config/remap.lua
@@ -5,6 +5,7 @@
 vim.g.mapleader = " "
 vim.keymap.set('', '<Space>', '<Nop>')
 
+
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -29,3 +29,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+
+# Created by `pipx` on 2026-02-28 08:38:48
+export PATH="$PATH:/home/hyzoor/.local/bin"
